b4726daac31f729032d8b7f025f8e82c210df62f
Scott Hyndman <scott@madewithfelt.com>
2020-01-22 17:21:53 -0600

    Add basic screenshot test
    Added the Piet example as a screenshot test

--- test/layout_grid_test.dart
+++ test/layout_grid_test.dart
@@ -5,8 +5,10 @@ import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:flutter_layout_grid/src/rendering/layout_grid.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../example/piet.dart';
+
 void main() {
-  testWidgets('Computes fixed intrinsic sizes', (WidgetTester tester) async {
+  testWidgets('Computes fixed intrinsic sizes', (tester) async {
     final grid = LayoutGrid(
       templateColumnSizes: [FixedTrackSize(10)],
       templateRowSizes: [FixedTrackSize(10)],
@@ -20,5 +22,13 @@ void main() {
     expect(renderObject.getMinIntrinsicHeight(double.infinity), 10);
     expect(renderObject.getMaxIntrinsicWidth(double.infinity), 10);
     expect(renderObject.getMaxIntrinsicHeight(double.infinity), 10);
+  });
+
+  testWidgets('Piet screenshot test', (tester) async {
+    await tester.pumpWidget(PietApp());
+    await expectLater(
+      find.byType(PietApp),
+      matchesGoldenFile('goldens/piet.png'),
+    );
   });
 }
