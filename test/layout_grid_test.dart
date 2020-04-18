558fd889a46c7f3ea1a8b0d2e16f7666ac34896c
Dao Hoang Son <daohoangson@gmail.com>
2020-04-18 09:44:29 +0700

    Add golden image

--- test/layout_grid_test.dart
+++ test/layout_grid_test.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:flutter_layout_grid/src/rendering/layout_grid.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -29,6 +29,65 @@ void main() {
     await expectLater(
       find.byType(PietApp),
       matchesGoldenFile('goldens/piet.png'),
+    );
+  });
+
+  testWidgets('Negative gap', (tester) async {
+    final borderWidth = 5.0;
+    final _box = ({Widget child, Color color}) => Container(
+          child: child,
+          decoration: BoxDecoration(
+            border: Border.all(width: borderWidth),
+            color: color,
+          ),
+        );
+
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            LayoutGrid(
+              columnGap: -borderWidth,
+              rowGap: -borderWidth,
+              gridFit: GridFit.passthrough,
+              templateColumnSizes: [
+                FlexibleTrackSize(1),
+                FlexibleTrackSize(1),
+                FlexibleTrackSize(1),
+              ],
+              templateRowSizes: [
+                IntrinsicContentTrackSize(),
+                IntrinsicContentTrackSize(),
+              ],
+              children: [
+                _box(color: Colors.red).withGridPlacement(
+                  columnStart: 0,
+                  rowStart: 0,
+                ),
+                _box(color: Colors.green).withGridPlacement(
+                  columnStart: 1,
+                  rowStart: 0,
+                ),
+                _box(color: Colors.blue).withGridPlacement(
+                  columnStart: 2,
+                  rowStart: 0,
+                ),
+                _box(color: Colors.purple).withGridPlacement(
+                  columnSpan: 2,
+                  columnStart: 0,
+                  rowStart: 1,
+                ),
+              ],
+            ),
+            Positioned.fill(child: _box()),
+          ],
+        ),
+      ),
+    ));
+
+    await expectLater(
+      find.byType(MaterialApp),
+      matchesGoldenFile('goldens/negative_gap.png'),
     );
   });
 }
