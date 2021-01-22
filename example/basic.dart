89879b5809abe6e8b5656cc50460397b53f66d0e
Scott Hyndman <scott@madewithfelt.com>
2021-01-21 20:18:13 -0500

    Remove outdated desktop setup code from example

--- example/basic.dart
+++ example/basic.dart
@@ -1,19 +1,7 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 
-// Sets a platform override for desktop to avoid exceptions. See
-// https://flutter.dev/desktop#target-platform-override for more info.
-void _enablePlatformOverrideForDesktop() {
-  if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
-    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-  }
-}
-
 void main() {
-  _enablePlatformOverrideForDesktop();
   runApp(MyApp());
 }
 
