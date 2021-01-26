a8224e9643ff373870f4b2b52d17c8f2a24a6f55
Scott Hyndman <scott@madewithfelt.com>
2021-01-25 23:34:34 -0500

    Add a debug flag for printing out unplaced children

--- lib/src/rendering/debug.dart
+++ lib/src/rendering/debug.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/widgets.dart';
 
-/// If [true], track sizing will be logged to the console via Flutter's
+/// If `true`, track sizing will be logged to the console via Flutter's
 /// [debugPrint] function.
 bool debugPrintGridLayout = false;
+
+/// If `true`, unplaced children will be logged to the
+bool debugPrintUnplacedChildren = false;
