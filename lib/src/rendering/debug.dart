ef02988a9b935f4111ae676bf207dc0123e44500
Scott Hyndman <scott@madewithfelt.com>
2021-02-02 16:49:50 -0500

    Add a bunch of debug logging
    It's still not perfect, but it's getting there.

--- lib/src/rendering/debug.dart
+++ lib/src/rendering/debug.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/widgets.dart';
 
+import 'layout_grid.dart';
+
 /// If `true`, track sizing will be logged to the console via Flutter's
 /// [debugPrint] function.
 bool debugPrintGridLayout = false;
@@ -7,3 +9,22 @@ bool debugPrintGridLayout = false;
 /// If `true`, unplaced children will be logged to the console via Flutter's
 /// [debugPrint] function.
 bool debugPrintUnplacedChildren = false;
+
+String debugTrackIndicesString(Iterable<GridTrack> tracks,
+    {bool trackPrefix = false}) {
+  final trackIndices = debugPrettyIndices(tracks.map((t) => t.index));
+
+  return tracks.isEmpty
+      ? trackIndices
+      : tracks.length == 1
+          ? 'track $trackIndices'
+          : 'tracks $trackIndices';
+}
+
+String debugPrettyIndices(Iterable<int> indices) {
+  return indices.isEmpty
+      ? '(none)'
+      : indices.length > 1
+          ? '[${indices.join(',')}]'
+          : '${indices.first}';
+}
