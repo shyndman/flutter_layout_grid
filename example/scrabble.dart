c0f4f37fe73e5ab647f897f2cf98ab1c4445e205
Scott Hyndman <scott@madewithfelt.com>
2021-02-04 08:50:06 -0500

    Correct alignment on Scrabble example's areas

--- example/scrabble.dart
+++ example/scrabble.dart
@@ -70,19 +70,19 @@ class ScrabbleBoard extends StatelessWidget {
     return LayoutGrid(
       areas: '''
         tw0 .  . dl0 .  .  . tw1 .  .  . dl1 .  . tw2
-          . dw0 .  .  . tl0 .  .  . tl1 .  .  . dw1 .
-          .  . dw2 .  .  . dl2 . dl3 .  .  . dw3 .  .
+         . dw0 .  .  . tl0 .  .  . tl1 .  .  . dw1 .
+         .  . dw2 .  .  . dl2 . dl3 .  .  . dw3 .  .
         dl4 .  . dw4 .  .  . dl5 .  .  . dw5 .  .  dl6
-          .  .  .  . dw6 .  .  .  .  . dw7 .  .  .  .
-          . tl2 .  .  . tl3 .  .  . tl4 .  .  . tl5 .
-          .  . dl7 .  .  . dl8 . dl9 .  .  . dla .  .
+         .  .  .  . dw6 .  .  .  .  . dw7 .  .  .  .
+         . tl2 .  .  . tl3 .  .  . tl4 .  .  . tl5 .
+         .  . dl7 .  .  . dl8 . dl9 .  .  . dla .  .
         tw3 .  . dlb .  .  .  ★  .  .  . dlc .  .  tw4
-          .  . dld .  .  . dle . dlf .  .  . dlg .  .
-          . tl6 .  .  . tl7 .  .  . tl8 .  .  . tl9 .
-          .  .  .  . dw8 .  .  .  .  . dw9 .  .  .  .
+         .  . dld .  .  . dle . dlf .  .  . dlg .  .
+         . tl6 .  .  . tl7 .  .  . tl8 .  .  . tl9 .
+         .  .  .  . dw8 .  .  .  .  . dw9 .  .  .  .
         dlh .  . dwa .  .  . dli .  .  . dwb .  .  dlj
-          .  . dwc .  .  . dlk . dll .  .  . dwd .  .
-          . dwe .  .  . tla .  .  . tlb .  .  . dwf .
+         .  . dwc .  .  . dlk . dll .  .  . dwd .  .
+         . dwe .  .  . tla .  .  . tlb .  .  . dwf .
         tw5 .  . dlm .  .  . tw6 .  .  . dln .  .  tw7
       ''',
       // A number of extension methods are provided for concise track sizing
