59b58add2d2a7549efccaa658a2f51f25f44dab7
Scott Hyndman <scott@madewithfelt.com>
2021-02-09 22:29:40 -0500

    Add hashCode to TrackSize subclasses

--- lib/src/rendering/track_size.dart
+++ lib/src/rendering/track_size.dart
@@ -172,6 +172,9 @@ class FixedTrackSize extends TrackSize {
   }
 
   @override
+  int get hashCode => hashValues(sizeInPx, debugLabel);
+
+  @override
   bool operator ==(dynamic other) {
     if (identical(this, other)) return true;
     if (other.runtimeType != runtimeType) return false;
@@ -242,6 +245,9 @@ class FlexibleTrackSize extends TrackSize {
   }
 
   @override
+  int get hashCode => hashValues(flex, debugLabel);
+
+  @override
   bool operator ==(dynamic other) {
     if (identical(this, other)) return true;
     if (other.runtimeType != runtimeType) return false;
@@ -295,6 +301,9 @@ class IntrinsicContentTrackSize extends TrackSize {
     );
     return max(maxContentContributions)!;
   }
+
+  @override
+  int get hashCode => debugLabel.hashCode;
 
   @override
   bool operator ==(dynamic other) {
